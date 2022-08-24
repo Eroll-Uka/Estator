714803f354db1b36b8c80a511d0f2d786f8faad9
Eroll Uka <74380038+Eroll-Uka@users.noreply.github.com>
2022-08-24 19:25:21 +0200

    listing item component patches

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,61 +1,91 @@
-import { Link } from 'react-router-dom'
-import { ReactComponent as DeleteIcon } from '../assets/svg/deleteIcon.svg'
-import bedIcon from '../assets/svg/bedIcon.svg'
-import bathtubIcon from '../assets/svg/bathtubIcon.svg'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { toast } from 'react-toastify'
+import Spinner from '../components/Spinner'
+import ListingItem from '../components/ListingItem'
 
-function ListingItem({ listing, id, onDelete }) {
+function Category() {
+  const [listings, setListings] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const params = useParams()
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        // Get reference
+        const listingsRef = collection(db, 'listings')
+
+        // Create a query
+        const q = query(
+          listingsRef,
+          where('type', '==', params.categoryName),
+          orderBy('timestamp', 'desc'),
+          limit(10)
+        )
+
+        // Execute query
+        const querySnap = await getDocs(q)
+
+        const listings = []
+
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          })
+        })
+
+        setListings(listings)
+        setLoading(false)
+      } catch (error) {
+        toast.error('Could not fetch listings')
+      }
+    }
+
+    fetchListings()
+  }, [params.categoryName])
+
   return (
-    <li className='categoryListing'>
-      <Link
-        to={`/category/${listing.type}/${id}`}
-        className='categoryListingLink'
-      >
-        <img
-          src={listing.imgUrls[0]}
-          alt={listing.name}
-          className='categoryListingImg'
-        />
-        <div className='categoryListingDetails'>
-          <p className='categoryListingLocation'>{listing.location}</p>
-          <p className='categoryListingName'>{listing.name}</p>
+    <div className='category'>
+      <header>
+        <p className='pageHeader'>
+          {params.categoryName === 'rent'
+            ? 'Places for rent'
+            : 'Places for sale'}
+        </p>
+      </header>
 
-          <p className='categoryListingPrice'>
-            $
-            {listing.offer
-              ? listing.discountedPrice
-                  .toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-              : listing.regularPrice
-                  .toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-            {listing.type === 'rent' && ' / Month'}
-          </p>
-          <div className='categoryListingInfoDiv'>
-            <img src={bedIcon} alt='bed' />
-            <p className='categoryListingInfoText'>
-              {listing.bedrooms > 1
-                ? `${listing.bedrooms} Bedrooms`
-                : '1 Bedroom'}
-            </p>
-            <img src={bathtubIcon} alt='bath' />
-            <p className='categoryListingInfoText'>
-              {listing.bathrooms > 1
-                ? `${listing.bathrooms} Bathrooms`
-                : '1 Bathroom'}
-            </p>
-          </div>
-        </div>
-      </Link>
-
-      {onDelete && (
-        <DeleteIcon
-          className='removeIcon'
-          fill='rgb(231, 76,60)'
-          onClick={() => onDelete(listing.id, listing.name)}
-        />
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className='categoryListings'>
+              {listings.map((listing) => (
+                <ListingItem 
+                listing={listing.data}
+                 id={listing.id} 
+                 key={listing.id}   />
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>No listings for {params.categoryName}</p>
       )}
-    </li>
+    </div>
   )
 }
 
-export default ListingItem
+export default Category

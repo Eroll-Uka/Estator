9b8ee680c4ee469e5a661d3cf3bfb144bb1eaa5d
Eroll Uka <74380038+Eroll-Uka@users.noreply.github.com>
2022-09-04 18:30:55 +0200

    Started Creating the  Listing Page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import { useState } from 'react'
 import {getAuth, updateProfile} from 'firebase/auth' 
 import {useNavigate} from 'react-router-dom'
 import {updateDoc, doc} from 'firebase/firestore'
 import {db} from '../firebase.config'
 import {toast} from 'react-toastify'
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
+import homeIcon from '../assets/svg/homeIcon.svg'
 
 
 function Profile() {
@@ -88,6 +91,12 @@ function Profile() {
           />
         </form>
       </div>
+
+      <Link to='/create-listing' className='createListing' >
+        <img src={homeIcon} alt="home" />
+        <p>Sell or rent your home</p>
+        <img src={arrowRight} alt="arrowRight" />
+      </Link>
     </main>
      </div>
 }

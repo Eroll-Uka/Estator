88cae80bc763765d09988ec806f9aa17f29f1deb
Eroll Uka <74380038+Eroll-Uka@users.noreply.github.com>
2022-10-06 16:33:52 +0200

    Finished Landlord page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -92,7 +92,7 @@ function Listing() {
       {/*map */}
 
       {auth.currentUser?.uid !== listing.userRef && (
-        <Link to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`} className='primaryButton'>
+        <Link to={`/contact/${listing.userRef}?listingName=${listing.name}`} className='primaryButton'>
           Contact Landlord
         </Link>
       )}
